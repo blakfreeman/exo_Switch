00d4d93fd3292cd18ccd3c015d37e99fd8e15420
blakfreeman <61966557+blakfreeman@users.noreply.github.com>
2021-04-20 16:53:57 +0200

    switch

---
+++ main.js
@@ -0,0 +1,14 @@
+var day = 'vendredi';
+
+
+switch (day) {
+    case "lundi":
+        console.log("On est lundi. Tu as cours  à MolenGeek et tu as sport en soirée.");
+        break;
+    case "vendredi":
+        console.log("le vendredi c'est boogie night");
+        break;
+    default:
+        console.log("on charbonne");
+        break;
+}
